0053264e012e15dedb210675d0bd597195d2b6ee
Vitalya <vitalya.2211@yandex.ru>
2022-06-28 04:26:47 +0400

    add 3 task

--- home_geek1.cpp
+++ home_geek1.cpp
@@ -8,8 +8,34 @@ int main()
 	cout <<  "insert number\n";
 	cin>>num;
 	num=(num%100)/10;
-	cout << num;
-	
+	cout << num<<endl;
+	/*выводит третью цифру заданного числа или сообщает что такой нет
+	4578=7
+	56=нет
+	564=4*/
+	cout <<"insert number3\n";
+	cin>>num;
+	if (num>99)
+	cout << "num";
+	else cout << "no\n";
+	cout << endl;
+	/*принимает цифру, обозначающую день недели и проверяет, 
+	является ли этот день выходным.
+	6->выходной
+	7- выходной
+	4 - нет*/
+	cout<<"insert num day\n";
+	cin>> num;
+	while(num>0&num<8)
+	{
+		if(num<6)
+		cout<<"work\n";
+		else cout << "weakend\n";
+		system("pause");
+		return 0;
+		
+	}
+	cout << "error! no day\n";
 	system ("pause");
 	return 0;
 }
